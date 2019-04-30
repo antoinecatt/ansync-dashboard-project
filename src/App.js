5bd8edc253e23daf30dd1ebb77505b911846f7dd
Antoine Catt <antoinecatt@gmail.com>
2019-04-30 13:20:15 -0700

    removed loading screen

--- src/App.js
+++ src/App.js
@@ -14,24 +14,24 @@ class App extends Component {
   componentDidMount() {
     axios
       .get('http://a.iot.ansync.com:8080/api/all')
-      .then(response => this.setState({ temps: response.data, isLoaded: true }));
+      .then(response =>
+        this.setState({ temps: response.data, isLoaded: true })
+      );
   }
 
   render() {
     return (
       <div>
-        <NavBar/>
+        <NavBar />
         <Container>
-          { this.state.isLoaded ?
-
+          {/* {this.state.isLoaded ? ( */}
             <TempCardList temps={this.state.temps} />
-            :
+          {/* ) : (
             <div>
-            <div className="loader" />
-            <h1>LOADING DASHBOARD...</h1>
-          </div>
-            
-          }
+              <div className="loader" />
+              <h1>LOADING DASHBOARD...</h1>
+            </div>
+          )} */}
         </Container>
       </div>
     );
