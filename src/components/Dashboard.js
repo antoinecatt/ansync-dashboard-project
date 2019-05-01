04bb3ce04c0a3cfa4f5ff30c6c492649ab149c3a
Antoine Catt <antoinecatt@gmail.com>
2019-04-30 17:30:11 -0700

    added axios and loading screen back

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
 import TempCardList from './TempCardList';
-import { temps } from '../temps';
-// import axios from 'axios';
+// import { temps } from '../temps';
+import axios from 'axios';
 
 class Dashboard extends Component {
   state = {
-    temps,
+    temps: [],
     isLoaded: false
   };
 
-  // componentDidMount() {
-  //   axios
-  //     .get('http://a.iot.ansync.com:8080/api/all')
-  //     .then(response =>
-  //       this.setState({ temps: response.data, isLoaded: true })
-  //     );
-  // }
+  componentDidMount() {
+    axios
+      .get('a.iot.ansync.com:8080/api/all')
+      .then(response =>
+        this.setState({ temps: response.data, isLoaded: true })
+      );
+  }
 
   render() {
     return (
       <div>
         <h1>DASHBOARD</h1>
-        <TempCardList temps={this.state.temps} />
-        {/* {this.state.isLoaded ? (
+        {this.state.isLoaded ? (
           <div>
             <h1>DASHBOARD</h1>
             <TempCardList temps={this.state.temps} />
@@ -32,7 +31,7 @@ class Dashboard extends Component {
             <div className="loader" />
             <h1>LOADING DASHBOARD...</h1>
           </div>
-        )} */}
+        )}
       </div>
     );
   }
