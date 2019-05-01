d49196c115c290d655a4f5cf01ac60769e93bae2
Antoine Catt <antoinecatt@gmail.com>
2019-04-30 17:25:24 -0700

    removed axios request and loading screen for deployment to work in heroku

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
 import TempCardList from './TempCardList';
-import axios from 'axios';
+import { temps } from '../temps';
+// import axios from 'axios';
 
 class Dashboard extends Component {
   state = {
-    temps: [],
+    temps,
     isLoaded: false
   };
 
-  componentDidMount() {
-    axios
-      .get('http://a.iot.ansync.com:8080/api/all')
-      .then(response =>
-        this.setState({ temps: response.data, isLoaded: true })
-      );
-  }
+  // componentDidMount() {
+  //   axios
+  //     .get('http://a.iot.ansync.com:8080/api/all')
+  //     .then(response =>
+  //       this.setState({ temps: response.data, isLoaded: true })
+  //     );
+  // }
 
   render() {
     return (
       <div>
-        {this.state.isLoaded ? (
+        <h1>DASHBOARD</h1>
+        <TempCardList temps={this.state.temps} />
+        {/* {this.state.isLoaded ? (
           <div>
             <h1>DASHBOARD</h1>
             <TempCardList temps={this.state.temps} />
@@ -29,7 +32,7 @@ class Dashboard extends Component {
             <div className="loader" />
             <h1>LOADING DASHBOARD...</h1>
           </div>
-        )}
+        )} */}
       </div>
     );
   }
