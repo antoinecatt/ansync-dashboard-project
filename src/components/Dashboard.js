49b2766ae0eec198f9229a1f464be97effdf89c9
Antoine Catt <antoinecatt@gmail.com>
2019-04-30 18:26:12 -0700

    added validation for Heroku version site

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,36 +1,37 @@
 import React, { Component } from 'react';
 import TempCardList from './TempCardList';
-// import { temps } from '../temps';
-import axios from 'axios';
+import { temps } from '../temps';
+// import axios from 'axios';
 
 class Dashboard extends Component {
   state = {
-    temps: [],
+    temps,
     isLoaded: false
   };
 
-  componentDidMount() {
-    axios
-      .get('http://a.iot.ansync.com:8080/api/all')
-      .then(response =>
-        this.setState({ temps: response.data, isLoaded: true })
-      );
-  }
+  // componentDidMount() {
+  //   axios
+  //     .get('http://a.iot.ansync.com:8080/api/all')
+  //     .then(response =>
+  //       this.setState({ temps: response.data, isLoaded: true })
+  //     );
+  // }
 
   render() {
     return (
       <div>
-        {this.state.isLoaded ? (
-          <div>
-            <h1>DASHBOARD</h1>
+                    <h1>DASHBOARD</h1>
             <TempCardList temps={this.state.temps} />
+        {/* {this.state.isLoaded ? (
+          <div>
+
           </div>
         ) : (
           <div>
             <div className="loader" />
             <h1>LOADING DASHBOARD...</h1>
           </div>
-        )}
+        )} */}
       </div>
     );
   }
